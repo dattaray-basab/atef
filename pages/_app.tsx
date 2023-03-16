1b6653e6807fa5cd6ee5d03fd9165ab411998768
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-03-16 16:47:04 -0700

    0.9.4: WIP: added experimental in next.config.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import type { Session } from "next-auth"
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
-export default function App({
+export default function App2({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
