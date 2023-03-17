199f1714b385eb458ea239849d28c13e8848bc1c
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-03-17 14:45:10 -0700

    0.9.5.6: EXPT - simplify auth

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./header.module.css";
-import AuthBar from "./auth_bar";
+import AuthBar from "./authBar";
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
