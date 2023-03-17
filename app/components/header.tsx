0b5b3bfe7b2cfd40a22a6efd97185d57c35d17ed
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-03-17 14:43:49 -0700

    0.9.5.6: EXPT - simplify auth

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,37 +1,18 @@
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./header.module.css";
+import AuthBar from "./auth_bar";
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Header() {
-  const { data: session } = useSession();
+
 
   return (
     <header>
-      <div>
-        {!session && (
-          <a
-            href={`/api/auth/signin`}
-            onClick={(e) => {
-              e.preventDefault();
-              signIn();
-            }}>
-            Sign in
-          </a>
-        )}
-        {session?.user && (
-          <a
-            href={`/api/auth/signout`}
-            onClick={(e) => {
-              e.preventDefault();
-              signOut();
-            }}>
-            Sign out
-          </a>
-        )}
-      </div>
+      <AuthBar />
+
       <nav>
         <ul className={styles.navItems}>
           <li className={styles.navItem}>
