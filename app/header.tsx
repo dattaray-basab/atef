ba1bfa0e5f8c4ecae1bb7145ae0830c045bf97a4
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-03-17 21:20:46 -0700

    0.9.5.11: EXPT - only home and admin

---
+++ app/header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "./header.module.css";
+import AuthBar from "./authBar";
+// import AuthBar from "./authBar";
+
+// The approach used in this component shows how to build a sign in and sign out
+// component that works on pages which support both client and server side
+// rendering, and avoids any flash incorrect content on initial page load.
+export default function Header() {
+
+
+  return (
+    <header>
+      <AuthBar />
+
+      <nav>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>
+            <Link href='/'>Home</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href='/admin'>Admin</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
