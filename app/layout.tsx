d57d03251d5c427fc2d5cb33315e29cf053084ee
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-03-19 03:01:52 -0700

    0.9.6.2: Start adding tailwind styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "./header";
 import type { ReactNode } from "react";
+import "../styles/globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
