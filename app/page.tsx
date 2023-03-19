b7c85e05e336b08a81fb1087ccd604a0da89b827
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-03-19 14:25:17 -0700

    0.9.6.4: Start adding tailwind styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-
-
 export default function Home() {
   return (
     <>
       <h1>NextAuth.js Example</h1>
-      <p>
+      <p className='text-6xl text-white font-bold mt-4'>
         This is an example site to demonstrate how to use{" "}
         <a href='https://next-auth.js.org'>NextAuth.js</a> for authentication.
       </p>
