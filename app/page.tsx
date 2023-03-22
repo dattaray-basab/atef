a76d3e71943b42db023ad78baa067ef465bdd876
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-03-21 21:06:13 -0700

    0.10.5: WIP - signin shows but does not change state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>NextAuth.js Example</h1>
+
       <p className='text-xl text-red-500 font-bold mt-4'>
         This is an example site to demonstrate how to use{" "}
         <a href='https://next-auth.js.org'>NextAuth.js</a> for authentication.
--- components/auth_session/authenticator.tsx
+++ components/auth_session/authenticator.tsx
@@ -1,32 +1,30 @@
 
 import { signIn, signOut, useSession } from "next-auth/react";
-import styles from "./authenticator.module.css";
+
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Authenticator() {
-  const { data: session, status } = useSession();
-  const loading = status === "loading";
+  const { data: session } = useSession();
+  // const loading = status === "loading";
 
   return (
     <header>
       <noscript>
         <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
       </noscript>
-      <div className={styles.signedInStatus}>
-        <p
-          className={`nojs-show ${
-            !session && loading ? styles.loading : styles.loaded
-          }`}>
+      <div >
+        <p>
           {!session && (
             <>
-              <span className={styles.notSignedInText}>
+              <span>
                 You are not signed in
               </span>
+              <br></br>
               <a
                 href={`/api/auth/signin`}
-                className={styles.buttonPrimary}
+                
                 onClick={(e) => {
                   e.preventDefault();
                   signIn();
@@ -40,17 +38,17 @@ export default function Authenticator() {
               {session.user.image && (
                 <span
                   style={{ backgroundImage: `url('${session.user.image}')` }}
-                  className={styles.avatar}
+                
                 />
               )}
-              <span className={styles.signedInText}>
+              <span >
                 <small>Signed in as</small>
                 <br />
                 <strong>{session.user.email ?? session.user.name}</strong>
               </span>
               <a
                 href={`/api/auth/signout`}
-                className={styles.button}
+              
                 onClick={(e) => {
                   e.preventDefault();
                   signOut();
