e36402340117ebaf7f1045b379443dd8d0fc5fe9
asidd123 <siddiquiatef10@gmail.com>
2023-04-16 17:13:36 -0700

    0.20.2: WIP - fixed project perspectives

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <>
 
-      <p className='text-xl text-red-500 font-bold mt-4'>
-        Atef's home page
+      <p className='text-lg text-emerald-600 font-mono mt-4 mb-5 ml-3 mr-3'>
+        Welcome to my Homepage! This site was part of a project I did where I wanted to have different pathways for people to learn different ways of creating websites. I had considered three modern ways of achieving that (Angular Vue and React) and I chose React as it seemed that an increasing number of developers were gravitating towards it of late. React is an open-source, front-end, Javascript framework developed by Facebook. I noticed that NextJS adds back-end capability making React a full stack environment. An alternative for the back-end could have been Remix. I considered different paths of styling this site such as CSS however it is quite low level. Another candidate was ChakraUI which has opinionated widgets which are react components. Finally, I embarked on Tailwind which seemed middle of the ground and well integrated with the React/NextJS framework. 
       </p>
     </>
   );
