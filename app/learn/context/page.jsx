7e83ca6af9765a4de13b837a8e14238505b6d88b
asidd123 <siddiquiatef10@gmail.com>
2023-04-29 21:32:23 -0700

    0.20.2: WIP - learning site work

---
+++ app/learn/context/page.jsx
@@ -0,0 +1,31 @@
+const StyleSheetsPage = () => {
+  return (
+    <section>
+      <div>
+        <h1><strong>Context</strong></h1>
+        <div>
+          <p><p>
+The history of web development can be traced back to the late 1980s when Tim Berners-Lee invented the World Wide Web (WWW) while working at CERN, the European Organization for Nuclear Research. The first website was launched in 1991, which contained basic information about the World Wide Web project.
+</p>
+<p>
+In the mid-1990s, the first commercial web browsers were introduced, including Netscape Navigator and Microsoft Internet Explorer. This led to a rapid increase in the popularity of the web, and businesses began to create their own websites to establish an online presence.
+</p>
+<p>
+In the early 2000s, web development technologies continued to evolve, with the introduction of dynamic web pages using technologies such as JavaScript and XML. Content management systems (CMS) like WordPress also emerged, making it easier for non-technical users to create and manage websites.
+</p>
+<p>
+The mid-2000s saw the rise of web 2.0 technologies, which enabled users to interact and collaborate with each other through social media, wikis, and blogs. Mobile devices also became popular, leading to the development of responsive web design to ensure websites could be accessed on different screen sizes.
+</p>
+<p>
+Each of these advancements have been caused by the rapidly increasing viewer demand. This had led to the explosion of evermore sophisticated frameworks and techniques. However, throughout all this progress there have been common standards such as HTML, HTTP, JavaScript, and Cascading Style Sheets. In this project I will be focusing on one way of building modern websites which uses state of the art technologies, keeping in mind that there are many frameworks available and new frameworks are constantly appearing to replace predecessors. 
+</p>
+<p>
+In researching these frameworks I ran across NextJS which is one of the leading technologies for building websites today. In fact this site is built using NextJS, which uses React for client side operations and works seamlessly with Tailwind as a styling component.
+</p></p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default StyleSheetsPage;
--- app/learn/leftBar.tsx
+++ app/learn/leftBar.tsx
@@ -5,19 +5,19 @@ const LeftBar = () => {
     <aside className='flex my-2 w-30 p-2 '>
       <ul className='flex flex-col '>
         <li>
-          <Link href='/learn/overview' >Project Overview</Link>
+          <Link href='/learn/overview' >Overview</Link>
         </li>
         <li>
-          <Link href='/learn/perspectives'>Personal Perspectives</Link>
+          <Link href='/learn/context'>Context</Link>
         </li>
         <li>
-          <Link href='/learn/style-sheets'>Style Sheets</Link>
+          <Link href='/learn/perspectives'>Perspectives</Link>
         </li>
         <li>
-          <Link href='/learn/react'>React</Link>
+          <Link href='/learn/timeline'>Timeline</Link>
         </li>
         <li>
-          <Link href='/learn/nextjs'>NextJS</Link>
+          <Link href='/learn/glossary'>Glossary</Link>
         </li>
       </ul>
     </aside>
