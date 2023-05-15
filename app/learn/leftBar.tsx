c20370a5dc1715afdc95e21c4bacba43bcfa1930
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-05-14 21:10:59 -0700

    0.20.5: WIP - added hover highlights for the links in the left nav bar for the Atef's Learning Site

--- app/learn/leftBar.tsx
+++ app/learn/leftBar.tsx
@@ -3,20 +3,20 @@ import Link from "next/link";
 const LeftBar = () => {
   return (
     <aside className='flex my-2 w-30 p-2 '>
-      <ul className='flex flex-col '>
-        <li>
-          <Link href='/learn/overview' >Overview</Link>
+      <ul className='text-xl text-slate-600 '>
+        <li className='hover:underline'>
+          <Link href='/learn/overview'>Overview</Link>
         </li>
-        <li>
+        <li className='hover:underline'>
           <Link href='/learn/context'>Context</Link>
         </li>
-        <li>
+        <li className='hover:underline'>
           <Link href='/learn/perspectives'>Perspectives</Link>
         </li>
-        <li>
+        <li className='hover:underline'>
           <Link href='/learn/timeline'>Timeline</Link>
         </li>
-        <li>
+        <li className='hover:underline'>
           <Link href='/learn/glossary'>Glossary</Link>
         </li>
       </ul>
